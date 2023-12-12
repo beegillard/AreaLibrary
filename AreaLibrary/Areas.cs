d5b86d5961e5fded26bbfa626f512f8cc6a53e63
Betsy Gillard <104951899+beegillard@users.noreply.github.com>
2023-12-11 19:09:51 -0700

    first commit

---
+++ AreaLibrary/Areas.cs
@@ -0,0 +1,21 @@
+﻿namespace Calculations;
+
+public static class AreaLibrary
+{
+    public static double CircleArea(double rad) {
+            double area = Math.PI * Math.Pow(rad, 2);
+            return area;   
+    }
+
+    public static double TriangleArea (double length, double height) {
+        double area = 0.5 * length * height;
+        return area;
+    }
+
+     public static double RectangleArea (double length, double width) {
+        double area = length * width;
+        return area;
+    }
+
+    
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Runtime.InteropServices;
+using Calculations;
+class Program
+{
+    static void Main(string[] args)
+    {
+        void GetInput() {
+            Console.WriteLine("Enter 'c' for circle, 'r' for rectangle, or 't' for triangle.");
+            string? input = Console.ReadLine();
+        
+            switch (input) {
+                case "c": 
+                    Console.WriteLine("enter radius:");
+                    double radius = Convert.ToDouble(Console.ReadLine());
+
+                    if (double.IsNaN(radius)) {
+                        throw new ArgumentException("Radius must be in the form of a number.");
+                    } else if (radius < 0) {
+                        throw new ArgumentException("Radius must be a positive number");
+                    } else {
+                        double area = AreaLibrary.CircleArea(radius);
+                        Console.WriteLine($"Area of the circle is {area}");
+                        }
+                    
+                    break;
+
+                case "r":
+                    Console.WriteLine("enter width");
+                    double width = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("enter height");
+                    double height = Convert.ToDouble(Console.ReadLine());
+                    double rarea = AreaLibrary.RectangleArea(width, height);
+                    Console.WriteLine($"The area of the rectangle is {rarea}");
+                    break;
+
+                case "t":
+                    Console.WriteLine("enter width");
+                    double twidth = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("enter height");
+                    double theight = Convert.ToDouble(Console.ReadLine());
+                    double tarea = AreaLibrary.TriangleArea(twidth, theight);
+                    Console.WriteLine($"The area of the triangle is {tarea}");
+                    break;
+                default:
+                    Console.WriteLine("Enter 'c', 't', or 'r'");
+                    break;
+            }
+
+
+        }
+
+        GetInput();
+
+
+          
+
+        
+
+      
+    
+
+      
+    }
+
+
+      
+}
+
